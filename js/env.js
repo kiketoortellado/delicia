fe7992603568d9348059ccbcf189f01c94540bb1
Gilberto Ortellado <kiketo.ortellado@gmail.com>
2026-02-19 14:31:39 -0300

    Delete js directory

--- js/env.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCdKSf2g1cIyXrD6k_u1aR9TMln9HaPJro",
-  authDomain: "delicia-restaurante.firebaseapp.com",
-  databaseURL: "https://delicia-restaurante-default-rtdb.firebaseio.com",
-  projectId: "delicia-restaurante",
-  storageBucket: "delicia-restaurante.firebasestorage.app",
-  messagingSenderId: "653903447900",
-  appId: "1:653903447900:web:85cffda9d703e464af6399"
-};
